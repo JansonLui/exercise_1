b6da67f5bb2c04f63e8349d91c778092c92b8fdd
JansonLui <jansonclui@gmail.com>
2016-06-18 22:23:35 +0000

    updating loading and modelling folder

--- loading_and_modelling/load_data_lake.sh
+++ loading_and_modelling/load_data_lake.sh
@@ -1,1 +1,9 @@
+#!/bin/bash
 
+file=$1
+tail -n+2 $file > ${file%????}
+
+hdfs dfs -mkdir /user/w205/hospital_compare/${file%????}
+hdfs dfs -put ${file%????} /user/w205/hospital_compare/${file%????}/$file
+
+rm ${file%????}
