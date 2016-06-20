b575deaf275697a9384452160b2e482589529539
JansonLui <jansonclui@gmail.com>
2016-06-20 12:25:57 +0000

    investigations

---
+++ investigations/best_states/best_states.sql
@@ -0,0 +1,2 @@
+select a.state, round(avg(b.score),2) as average from hospitals_mod a right outer join effective_care_mod b on a.provider_id = b.provider_id 
+where b.score > 0 group by a.state order by average desc limit 10;
