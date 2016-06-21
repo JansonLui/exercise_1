f67555fdf2482ec65646b2f6431de685cd047d7a
JansonLui <jansonclui@gmail.com>
2016-06-21 10:35:23 -0700

    Delete readmissions_mod.sql

--- transforming/readmissions_mod.sql
+++
@@ -1,2 +0,0 @@
-create table if not exists readmissions_mod
-as select provider_id, measure_id, compared_to_national, denominator, score, lower_estimate, higher_estimate, footnote from readmissions;
