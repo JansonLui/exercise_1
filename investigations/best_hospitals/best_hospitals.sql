1786926f0b9aa548797e63b6afcd6ba1c1ac5ab9
JansonLui <jansonclui@gmail.com>
2016-06-21 23:44:51 +0000

    update

--- investigations/best_hospitals/best_hospitals.sql
+++ investigations/best_hospitals/best_hospitals.sql
@@ -1,2 +1,3 @@
-select a.hospital_name, max(b.score), min(b.score), sum(b.score), round(avg(b.score),2) as average from hospitals_mod a 
-right outer join effective_readmissions_care_mod b on a.provider_id = b.provider_id where b.score > 0 group by a.hospital_name order by average desc limit 10;
+select a.hospital_name, max(b.score), min(b.score), round(sum(b.score),2), round(avg(b.score),2) as average from hospitals_mod a
+right outer join effective_readmissions_care_mod b on a.provider_id = b.provider_id where b.score > 0
+group by a.hospital_name order by average desc limit 10;
